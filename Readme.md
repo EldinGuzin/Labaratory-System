2096208ed13fe91d8da28fa68fce6460f93c2ef5
Eldin Guzin <pelda541@gmail.com>
2024-04-21 21:30:43 +0200

    reading from the results table

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Laboratory Information System
+
+Welcome to the Laboratory Information System (LIS)!
+
+## Introduction
+
+The Laboratory Information System is a web-based application that allows users to enter and view their laboratory test results. It provides a convenient and secure way for users to access their data from anywhere, at any time.
+
+## Getting Started
+
+To get started with the Laboratory Information System, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Install the necessary dependencies.
+3. Configure the database connection settings.
+4. Run the application and access it through your web browser.
+
+
+
--- live-backend/rest/dao/TestResultsDao.class.php
+++ live-backend/rest/dao/TestResultsDao.class.php
@@ -11,6 +11,23 @@ class TestResultsDao extends BaseDao {
     public function insert($data) {
         return parent::insert($data);
     }
+
+    public function getAll($id = null) {
+        
+        $query = "SELECT name, surname, username, hemoglobin, rbc_count, pcv, mcv, tlc, neutrophils, lymphocytes, monocytes, eosinophils, basophils, additional_note FROM test_results";
+    
+        
+        if ($id !== null) {
+            $query .= " WHERE id = :id";
+        }
+    
+        
+        $results = $this->query($query, [':id' => $id]);
+    
+        
+        return $results;
+    }
+    
     
     // Add more methods here for other CRUD operations if needed
 }
