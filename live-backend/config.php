5842d912e321d3f431deea342531f5f4c00c211c
Eldin Guzin <pelda541@gmail.com>
2024-06-03 22:02:08 +0200

    config issue

--- live-backend/config.php
+++ live-backend/config.php
@@ -5,15 +5,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
-// Database access credentials
-// define('DB_NAME', 'Labaratory_management_system');
-// define('DB_PORT', 3306);
-// define('DB_USER', 'root');
-// define('DB_PASSWORD', '541Lopov@');
-// define('DB_HOST', '127.0.0.1'); // localhost
-// define('JWT_SECRET', '1234567890');
-
-
 
 class Config {
     public static function DB_NAME() {
